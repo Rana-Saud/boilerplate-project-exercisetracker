57eb8c24251857a5992633837cbc15aead2dfd2d
Rana Saud <126066239+Rana-Saud@users.noreply.github.com>
2024-12-28 05:58:04 -0800

    fix typo in comment for middleware section

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require('cors')
 const { v4: uuidv4 } = require('uuid');
 require('dotenv').config()
 
-// MODDLEWARES
+// MIDDLEWARES
 app.use(cors())
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
